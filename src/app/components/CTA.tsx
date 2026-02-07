c0c991bddd825d5767fe660fe9e95047b5f214c4
TypeErrorDev <79867801+TypeErrorDev@users.noreply.github.com>
2026-02-06 19:30:48 -0800

    [feat] creating the waitlist as the '/' page

---
+++ src/app/components/CTA.tsx
@@ -0,0 +1,14 @@
+function CTA() {
+  return (
+    <div>
+      <h1 className="mt-10 text-black font-extrabold text-[72px] ">
+        Track Projects With
+        <span className="block text-[#8743FF] text-shadow-lg/40">
+          Confidence
+        </span>
+      </h1>
+    </div>
+  );
+}
+
+export default CTA;
---
+++ src/app/components/Waitlist.tsx
@@ -0,0 +1,13 @@
+import CTA from "./CTA";
+import WaitlistTopBar from "./WaitlistTopBar";
+
+function Waitlist() {
+  return (
+    <>
+      <WaitlistTopBar />
+      <CTA />
+    </>
+  );
+}
+
+export default Waitlist;
