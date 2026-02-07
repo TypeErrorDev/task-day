cb4e882e95e9686c07c04de39f23fac44d69a265
TypeErrorDev <79867801+TypeErrorDev@users.noreply.github.com>
2026-02-06 20:11:50 -0800

    [feat] Working on the CTA component of the waitlist

--- src/app/components/CTA.tsx
+++ src/app/components/CTA.tsx
@@ -1,12 +1,23 @@
 function CTA() {
   return (
-    <div>
-      <h1 className="mt-10 text-black font-extrabold text-[72px] ">
+    <div className="flex flex-col items-center outline outline-slate-300/30 h-">
+      <h1 className="mt-10 text-5xl font-extrabold tracking-tight text-slate-900 text-[72px] text-center ">
         Track Projects With
+        <br />
         <span className="block text-[#8743FF] text-shadow-lg/40">
           Confidence
         </span>
       </h1>
+      <p className="text-[#535151] font-bold  text-start mt-20 text-[24px]">
+        Streamline your workflow, boost productivity, and <br />
+        never miss a deadline. The ultimate project tracking <br /> solution for
+        developers, teams and hobbyists alike!!
+      </p>
+      <div>
+        <button className="text-white bg-linear-90 from-[#1B0EF1] to-[#8743FF] rounded-lg w-75 font-bold text-2xl py-1 my-20">
+          Get Started
+        </button>
+      </div>
     </div>
   );
 }
